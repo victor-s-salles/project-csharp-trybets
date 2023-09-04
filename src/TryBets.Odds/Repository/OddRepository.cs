5688121f49d07774c6362a0bbc030778304a6ab2
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-09-04 19:03:32 -0300

    Feat: Req 4 e dockerfile dos microservicos

--- src/TryBets.Odds/Repository/OddRepository.cs
+++ src/TryBets.Odds/Repository/OddRepository.cs
@@ -15,6 +15,23 @@ public class OddRepository : IOddRepository
 
     public Match Patch(int MatchId, int TeamId, string BetValue)
     {
-        throw new NotImplementedException();
+        Match matchDb = _context.Matches.FirstOrDefault(m => m.MatchId == MatchId)!;
+
+        if (matchDb == null) throw new Exception("Match not founded");
+
+        Team findedTeam = _context.Teams.FirstOrDefault(t => t.TeamId == TeamId)!;
+        if (findedTeam == null) throw new Exception("Team not founded");
+
+        if (matchDb.MatchFinished) throw new Exception("Match finished");
+
+        if (matchDb.MatchTeamAId != TeamId && matchDb.MatchTeamBId != TeamId) throw new Exception("Team is not in this match");
+
+        if (matchDb.MatchTeamAId == TeamId) matchDb.MatchTeamAValue += decimal.Parse(BetValue);
+        else matchDb.MatchTeamBValue += decimal.Parse(BetValue);
+
+        _context.Matches.Update(matchDb);
+        _context.SaveChanges();
+
+        return matchDb;
     }
 }
